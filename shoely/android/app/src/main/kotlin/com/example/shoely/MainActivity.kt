1898e275c12425f517eafd6cbc1ba8b1bb662ce7
sonukarmacharya44 <sonu.karmacharya@upaya.com.np>
2024-06-27 18:50:40 +0545

    added folders

---
+++ shoely/android/app/src/main/kotlin/com/example/shoely/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shoely
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ shoely/lib/utils/font/app_text_style.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  static const heading900 = TextStyle(
+    fontSize: 48,
+    fontFamily: 'inter',
+    fontWeight: FontWeight.w900,
+  );
+
+static const heading800 = TextStyle(
+    fontSize: 48,
+    fontFamily: 'inter',
+    fontWeight: FontWeight.w900,
+    
+  );
+
+  
+}
